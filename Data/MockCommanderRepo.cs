28f52d82386d08537382e9a79d4f41552d343956
edindzabija <edindzabija@gmail.com>
2020-09-07 20:26:48 +0200

    initial commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Get your external IP address", Line = "curl ifconfig.me", Platform = "Linux" },
+                new Command { Id = 1, HowTo = "Run the last command as root", Line = "sudo !!", Platform = "Linux" },
+                new Command { Id = 2, HowTo = "Clear terminal screen", Line = "ctrl+l", Platform = "Linux" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Display top ten running processes - sorted by memory usage", Line = "ps aux | sort -nk +4 | tail", Platform = "Linux" };
+        }
+    }
+}
